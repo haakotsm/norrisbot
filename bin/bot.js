a690f14c2d4547496c2523dc369bc0bc75336ecc
Håkon Smørvik <hakon.smoervik@live.no>
2017-06-21 12:40:15 +0200

    Update bot.js

--- bin/bot.js
+++ bin/bot.js
@@ -2,13 +2,13 @@
  * Created by hakon.smorvik on 20.06.2017.
  */
 'use strict';
-let SverreBot = require('../lib/sverrebot');
+let NorrisBot = require('../lib/norrisbot');
 
 let token = process.env.BOT_API_KEY;
 let dbPath = process.env.BOT_DB_PATH;
 let name = process.env.BOT_NAME;
 
-let sverrebot = new SverreBot({
+let sverrebot = new NorrisBot({
     token: token,
     dbPath: dbPath,
     name: name || 'norrisbot'
